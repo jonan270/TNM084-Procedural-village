4773deded0e5d9d514ce2c6a8392165a2ba228e4
jonan270 <57438307+jonan270@users.noreply.github.com>
2021-12-17 16:27:22 +0100

    Basic project setup

---
+++ cmake-build-debug/CMakeFiles/TNM084_Procedural_village.dir/DependInfo.cmake
@@ -0,0 +1,46 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/jonathanandersson/Documents/Git/Procedural images/TNM084-Procedural-village/common/GL_utilities.c" "/Users/jonathanandersson/Documents/Git/Procedural images/TNM084-Procedural-village/cmake-build-debug/CMakeFiles/TNM084_Procedural_village.dir/common/GL_utilities.c.o"
+  "/Users/jonathanandersson/Documents/Git/Procedural images/TNM084-Procedural-village/common/LittleOBJLoader.c" "/Users/jonathanandersson/Documents/Git/Procedural images/TNM084-Procedural-village/cmake-build-debug/CMakeFiles/TNM084_Procedural_village.dir/common/LittleOBJLoader.c.o"
+  "/Users/jonathanandersson/Documents/Git/Procedural images/TNM084-Procedural-village/common/LoadTGA.c" "/Users/jonathanandersson/Documents/Git/Procedural images/TNM084-Procedural-village/cmake-build-debug/CMakeFiles/TNM084_Procedural_village.dir/common/LoadTGA.c.o"
+  "/Users/jonathanandersson/Documents/Git/Procedural images/TNM084-Procedural-village/common/Mac/MicroGlut.m" "/Users/jonathanandersson/Documents/Git/Procedural images/TNM084-Procedural-village/cmake-build-debug/CMakeFiles/TNM084_Procedural_village.dir/common/Mac/MicroGlut.m.o"
+  "/Users/jonathanandersson/Documents/Git/Procedural images/TNM084-Procedural-village/common/VectorUtils3.c" "/Users/jonathanandersson/Documents/Git/Procedural images/TNM084-Procedural-village/cmake-build-debug/CMakeFiles/TNM084_Procedural_village.dir/common/VectorUtils3.c.o"
+  "/Users/jonathanandersson/Documents/Git/Procedural images/TNM084-Procedural-village/noise/noise1234.c" "/Users/jonathanandersson/Documents/Git/Procedural images/TNM084-Procedural-village/cmake-build-debug/CMakeFiles/TNM084_Procedural_village.dir/noise/noise1234.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../common"
+  "../noise"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/jonathanandersson/Documents/Git/Procedural images/TNM084-Procedural-village/main.cpp" "/Users/jonathanandersson/Documents/Git/Procedural images/TNM084-Procedural-village/cmake-build-debug/CMakeFiles/TNM084_Procedural_village.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../common"
+  "../noise"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
